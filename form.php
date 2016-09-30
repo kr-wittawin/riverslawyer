ad857877fb3a81c012d979262aa2da52b994b54f
Ben Wongseelashote <ben41381@hotmail.com>
2016-09-30 21:41:24 +1000

    form complete w/o validation

--- form.php
+++ form.php
@@ -4,7 +4,6 @@
 <!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-<!--http://thecodeplayer.com/walkthrough/jquery-multi-step-form-with-progress-bar-->
 
 	<head>
 		<!-- meta character set -->
@@ -23,15 +22,18 @@
 
 		<!-- CSS-->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-
 		<!-- Fontawesome Icon font -->
 	    <link rel="stylesheet" href="css/font-awesome.css">
 		<!-- bootstrap.min -->
 		<link rel="stylesheet" href="css/bootstrap.css">
+		<!--bootstrap datepicker -->
+		<link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
 		<!-- Tooltipster -->
 		<link rel="stylesheet" href="css/tooltipster.bundle.min.css">
-		<!-- bootstrap.min -->
+		<!-- animate -->
 		<link rel="stylesheet" href="css/animate.css">
+		<!-- jquery UI -->
+	    <link rel="stylesheet" href="css/jquery-ui.css">
 		<!-- Main Stylesheet -->
 		<link rel="stylesheet" href="css/main.css">
 
@@ -99,11 +101,12 @@
 			<div class= "container formSize">
 
 				<!-- multistep form -->
-				<form method = "post" action = "formfilled.php" id="msform" >
+				<form method = "post" action = "parkingInfringementReviewLetter.php" id="msform" >
 					<!-- progressbar -->
 					<ul id="progressbar">
 						<li class="active">Parking Infringement Reason</li>
-						<li>Details</li>
+						<li>Personal Details</li>
+						<li>Infringement Notice Details</li>
 						<li>Donate</li>
 						<li>Parking Infringement Review Letter</li>
 					</ul>
@@ -112,19 +115,19 @@
 						<fieldset>
 							<h2 class="fs-title">Parking Fine Infringement Review (Victoria Only)</h2>
 							<h3 class="fs-subtitle">Please select the most appropriate scenario below.</h3>
-							<div class="infringement_reason">
-					        <input type='radio' name='infringementOption' id='infringement1' value='Option 1'/>
-					        <label for='infringement1'>My vehicle broke down.</label></br>
-					        <input type='radio' name='infringementOption'  id='infringement2' value='Option 2' />
-					        <label for='infringement2'>I was dealing with a medical emergency.</label><br/>
-					        <input type='radio'  name='infringementOption'  id='infringement3' value='Option 3' />
-					        <label for='infringement3'>The offence was before I bought the vehicle.</label><br/>
-							<input type='radio'  name='infringementOption'  id='infringement4' value='Option 4' />
-							<label for='infringement4'>The offence was after I sold the vehicle.</label><br/>
-							<input type='radio'  name='infringementOption'  id='infringement5' value='Option 5' />
-							<label for='infringement5'>The offence was after the vehicle was stolen.</label><br/>
-							<input type='radio'  name='infringementOption'  id='infringement6' value='Option 6' />
-							<label for='infringement6'>The parking sign was not visible.</label><br/>
+							<div class="infringementOption">
+						        <input type='radio' name='infringementOption' id='infringement1' value='Option 1'/>
+						        <label for='infringement1'>My vehicle broke down.</label></br>
+						        <input type='radio' name='infringementOption'  id='infringement2' value='Option 2' />
+						        <label for='infringement2'>I was dealing with a medical emergency.</label><br/>
+						        <input type='radio'  name='infringementOption'  id='infringement3' value='Option 3' />
+						        <label for='infringement3'>The offence was before I bought the vehicle.</label><br/>
+								<input type='radio'  name='infringementOption'  id='infringement4' value='Option 4' />
+								<label for='infringement4'>The offence was after I sold the vehicle.</label><br/>
+								<input type='radio'  name='infringementOption'  id='infringement5' value='Option 5' />
+								<label for='infringement5'>The offence was after the vehicle was stolen.</label><br/>
+								<input type='radio'  name='infringementOption'  id='infringement6' value='Option 6' />
+								<label for='infringement6'>The parking sign was not visible.</label><br/>
 							</div></br>
 							<div class="form-errors"></div><br/>
 							<input type="button" name="next" class="next action-button" value="Next" />
@@ -133,43 +136,45 @@
 							<h2 class="fs-title">Please Fill in Your Personal Details</h2>
 							<h3 class="fs-subtitle"></h3>
 							<div class = "personalDetail">
-							<select name = "title">
-								<option value="" disabled selected hidden>*Title</option>
-								<option value = "Mr.">Mr.</option>
-								<option value = "Ms.">Ms.</option>
-								<option value = "Mrs.">Mrs.</option>
-								<option value = "Dr.">Dr.</option>
-							</select>
-							<input type="text" name="firstName" id= "firstName" placeholder="*Given Name" size="30" maxlength = "30" />
-							<input type="text" name="lastName" id = "lastName" placeholder="*Family Name" size="30" maxlength = "30" />
-							<input type="tel" name="mobilePhone" id = "mobilePhone" placeholder="*Primary Contact Number" size="30" maxlength = "10" />
-							<input type="text" name="email" id = "email" placeholder="*Email Address" />
-							<input type="text" name="address" id = "address" placeholder="*Address" size="30" maxlength = "30" />
-							<input type="text" name="suburb" id = "suburb" placeholder="*Suburb" size="30" maxlength = "30" />
-							<input type="tel" name="postcode" id = "postcode" placeholder="*postcode" size="30" maxlength = "4" />
-							<select name = "state">
-								<option value="" disabled selected hidden>*State</option>
-								<option name="act">Australian Capital Territory</option>
-								<option name="nsw">New South Wales</option>
-								<option name="nt">Northern Territory</option>
-								<option name="qld">Queensland</option>
-								<option name="sa">South Australia</option>
-								<option name="tas">Tasmania</option>
-								<option name="vic">Victoria</option>
-								<option name="wa">Western Australia</option>
-							</select>
-							</div>
-							<h2 class="fs-title">Please fill in your Infringment Notice Details</h2>
-							<h3 class="fs-subtitle"></h3>
-							<div class = "infringementNoticeDetail">
-							<input type="text" name="infNo" id= "infNo" placeholder="*Infringement Notice Number" size="30" maxlength = "30" />
-							<input type="text" name="regNo" id= "regNo" placeholder="*Registration Number" size="30" maxlength = "20" />
-							<input type="text" name = "infDate" id="infDate" placeholder="*Infringement Date"  onfocus="(this.type='date')" onblur="(this.type='text')" />
-							<input type="text" name="infLocation" id= "infLocation" placeholder="*Location" size="30" maxlength = "60" />
+								<select name = "title">
+									<option value="" disabled selected hidden>*Title</option>
+									<option value = "Mr.">Mr.</option>
+									<option value = "Ms.">Ms.</option>
+									<option value = "Mrs.">Mrs.</option>
+									<option value = "Dr.">Dr.</option>
+								</select>
+								<input type="text" name="givenName" id= "givenName" placeholder="*Given Name" size="30" maxlength = "30" x-autocompletetype="given-name" />
+								<input type="text" name="familyName" id = "familyName" placeholder="*Family Name" size="30" maxlength = "30" x-autocompletetype="family-name" />
+								<input type="tel" name="mobilePhone" id = "mobilePhone" placeholder="*Primary Contact Number" size="30" maxlength = "10" x-autocompletetype="tel" />
 							</div>
 							<div class="form-errors"></div><br/>
-							<input type="button" name="next" class="next action-button" value="Submit Details" />
+							<input type="button" name="next" class="next action-button" value="Next" />
 							<input type="button" name="previous" class="previous action-button" value="Previous" />
+						</fieldset>
+						<fieldset>
+							<h2 class="fs-title">Please fill in your Infringement Notice Details</h2>
+							<h3 class="fs-subtitle"></h3>
+							<div class = "infringementNoticeDetail">
+								<input type="text" name="infNo" id= "infNo" placeholder="*Infringement Notice Number" size="30" maxlength = "30" />
+								<input type="text" name="regNo" id= "regNo" placeholder="*Registration Number" size="30" maxlength = "20" />
+								<input type="text" name ="infDate" id="infDate" placeholder="*Infringement Date (DD/MM/YYYY)" />
+								<input type="text" name="infstreet-address" id= "infstreet-address" placeholder="*Infringement Location" size="30" maxlength = "30" />
+								<input type="text" name="infsuburb-address" id = "infsuburb-address" placeholder="*Infringement Suburb" size="30" maxlength = "30" />
+								<select name = "state">
+									<option value="" disabled selected hidden>*Infringement State</option>
+									<option value="ACT">Australian Capital Territory</option>
+									<option value="NSW">New South Wales</option>
+									<option value="NT">Northern Territory</option>
+									<option value="QLD">Queensland</option>
+									<option value="SA">South Australia</option>
+									<option value="TAS">Tasmania</option>
+									<option value="VIC">Victoria</option>
+									<option value="WA">Western Australia</option>
+								</select>
+							</div>
+							<div class="form-errors"></div><br/>
+							<input type="button" name="next" class="next action-button" value = "Submit Details" />
+							<input type="button" name="previous" class="previous action-button" value = "Previous" />
 						</fieldset>
 						<fieldset>
 							<h2 class="fs-title">Donate to Charity</h2>
@@ -182,7 +187,7 @@
 							<input type="hidden" id="stripeToken" name="stripeToken" />
 							<input type="hidden" id="stripeEmail" name="stripeEmail" />
 							<input type="button" id="customCharge" class = "action-button" value = "Donate" /> <br/>
-							<input type="button" name="previous" class="previous action-button" value="Edit Details" />
+							<input type="button" name="previous" class="previous action-button" value = "Edit Details" />
 						</fieldset>
 					</div>
 				</form>
@@ -209,6 +214,15 @@
 		<script type="text/javascript" src="js/modernizr-2.6.2.min.js" type="text/javascript"></script>
 		<!-- Main jQuery -->
 		<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+		<!-- jQuery UI -->
+		<script src="js/jquery-ui.js" type="text/javascript"></script>
+		<script>
+		$(function() {
+			$("#infDate").datepicker({
+				dateFormat: "dd-mm-yy"
+			});
+		});
+		</script>
 		<!-- Twitter Bootstrap -->
 		<script src="js/bootstrap.min.js" type="text/javascript"></script>
 		<!-- Single Page Nav -->
@@ -219,7 +233,7 @@
 		<script src="js/jquery.validate.min.js" type="text/javascript" type="text/javascript"></script>
 		<script src="js/additional-methods.min.js" type="text/javascript"></script>
 		<!--tooltipster.core.min.js-->
-		<script src="js/tooltipster.bundle.min.js" type="text/javascript" type="text/javascript"></script>
+		<script src="js/tooltipster.bundle.min.js" type="text/javascript"></script>
 		<!--form_function.js-->
 		<script src="js/form_function.js" type="text/javascript" type="text/javascript"></script>
 		<!-- Custom Functions -->
